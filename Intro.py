862706aaab253fcf374354d6937a2234469bcb80
Egor <scarsunny2@gmail.com>
2025-06-26 19:36:48 +0500

    commit

--- Intro.py
+++ Intro.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-#CEATING OBJECTS/DATA
-#DataFrame = multidimensional array
-#Series = one dimensional arrays
+#CREATING OBJECTS/DATA
+# DataFrame = multidimensional array
+# Series = one dimensional arrays
 s = pd.Series([1,2,3,4,5])
 dates = pd.date_range('20250101', periods=6) # using pd.date_range() creates range of dates
 df = pd.DataFrame(np.random.randn(6,4), index = dates, columns = list('ABCD')) # using pd.DataFrame() we pass values, indexes and columns
@@ -33,12 +33,12 @@ df.sort_values(by='B')# DataFrame.sort_index() sorts indexes, DataFrame.sort_val
 df['A']# selects all 'A' column values
 df[1:4]# selects values of all columns from index 1 to index 3
 
-#df.loc['some index']# shows rows of that index/indexes IT WORKS - df.loc[indexes wanted,[columns wanted]]
+# df.loc['some index']# shows rows of that index/indexes IT WORKS - df.loc[indexes wanted,[columns wanted]]
 
-# DataFrame.iloc[indexes, columns] shows indexes and columns
+# DataFrame.iloc[indexes, columns] index row
 # DataFrame.iat[index, column] gives one scalar value fast
 
-#df[df['B'] < 0] boolean indexing shows all indexes and rows that are TRUE with statement
+# df[df['B'] < 0] boolean indexing shows all indexes and rows that are TRUE with statement
 
 df3 = df.copy()
 df3['E'] = ['Egor','Egor','Egor','Ksyusha','Ksyusha','Ksyusha']
@@ -53,3 +53,43 @@ df3[df3['E'].isin(['Ksyusha'])] # isin() shows indexes and columns if it has cer
 # df.fillna(n) fills NAN values with n value
 
 # pd.isna(df) checks if value is NAN in DataFrame return True or False
+
+#OPERATIONS
+
+# DatFrame.sub(what to substract,axis) IT MEANS = we subtract some values from wanted axis
+
+# DataFrame.index = DataFrame.MultiIndex.from_tuples([],names =[]) creates multiindex DataFrame
+
+# divmod(a,b) return tuples of a//b, a%b
+
+
+# STATS
+
+my = pd.DataFrame({
+    'first':[1,1,100,250],
+    'second':[3,3,100,250],
+    'third':[3,3,100,250],
+    'fourth':[5,5,100,250],
+    'fifth':[5,5,100,250],
+},index=['june','july','august','september'])
+
+# DataFrame.mean(axis) calculates meanvalue, if axis = 1 it calculates all meanvalues of indexes, otherwise - columns
+
+# DataFrame.agg(function) adds function operations to all elements of DataFrame/ can be used to return single value per column
+
+# DataFram.transform(func) can be used to transform each value in DataFrame
+
+# DataFrame.value_counts() counts how many of different values there are in columns
+
+# STRING OPERATIONS
+
+# Series.str.lower() makes all str's in upper casebecome lowercase
+
+# MERGE concat and join
+
+#pd.concat([]) it merges pieces of DataFrame or Series into one
+
+#pd.merge(a,b,on) merges a and b DataFrames or Series into one DataFrame or Series
+
+# Grouping
+
